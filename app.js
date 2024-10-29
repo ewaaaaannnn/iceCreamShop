757bde7fa78ab4dde24d5508ee367140ee7569d3
ewaaaaannnn <ewanferg01@gmail.com>
2024-10-29 15:22:31 -0600

    Fully Functional Cards and Checkout

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ function addItem(itemIndex) {
   let selectedItem = iceCream[itemIndex]
   selectedItem.quantity += 1
   console.log('selectedItem', selectedItem)
+
+
+  drawCart()
 }
 
 function calculateCartTotal() {
@@ -21,5 +24,45 @@ function calculateCartTotal() {
     total += item.price * item.quantity
   }
   console.log('total', total)
-  return total
+  return total;
+}
+
+function checkout() {
+  const cartTotal = calculateCartTotal()
+  const confirmed = confirm(`Are you sure you want to purchase your ice cream with a total of: ${cartTotal}`)
+  if (confirmed)
+    for (let i = 0; i < iceCream.length; i++) {
+      let item = iceCream[i]
+      item.quantity = 0
+    }
+  drawCart()
+
+}
+
+
+
+function drawCart() {
+  let content = ''
+  for (let i = 0; i < iceCream.length; i++) {
+    let item = iceCream[i]
+    if (item.quantity > 0) {
+      content += `
+    <p class="d-flex justify-content-between">
+      <span>${item.quantity}x ${item.name}</span>
+      <span class="text-success"><i class="mdi mdi-currency-usd"></i>${item.price * item.quantity}</span>
+    </p>`
+    }
+  }
+  console.log('content', content);
+  const cartElm = document.getElementById('cart-display')
+  cartElm.innerHTML = content
+  drawTotal()
+}
+
+
+
+function drawTotal() {
+  let cartTotal = calculateCartTotal()
+  const totalElm = document.getElementById('cart-total')
+  totalElm.innerText = cartTotal.toFixed(2)
 }
